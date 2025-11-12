fcf02f03866e65b25bf0b96e44384a42195f92e9
Yaman SEVS <yamangurung4106@gmail.com>
2025-11-12 22:06:39 +1100

    Fix build: deps, tsconfig, next config, public assets

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+module.exports = {
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }],
+  },
 };
-
-export default nextConfig;
